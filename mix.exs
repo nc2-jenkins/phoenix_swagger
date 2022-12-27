a9a6e1e3d0e6d19a59a5af8c0ddce50c237c810a
Nikola Mladenovic <nikola.mladenovic@nutanix.com>
2022-12-27 14:19:12 +0100

    Set elixir version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule PhoenixSwagger.Mixfile do
     [
       app: :phoenix_swagger,
       version: @version,
-      elixir: "~> 1.8",
+      elixir: "1.9.1",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
